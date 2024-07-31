0fd48d6b2982f758498edc519872e4eb9ee80868
KingOfKings01 <aasifkhan9605@gmail.com>
2024-07-31 23:48:14 +0530

    added css and configur public static folder Html page created and link css files with them. countact page add with success.

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const adminRoutes = require("./routes/admin");
 const shopRouters = require("./routes/shop");
+const contactRouters = require("./routes/contact");
+const successRouters = require("./routes/success");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")))
 
 //todo apply root path with routes Note: '/' is important before name
 app.use('/admin', adminRoutes);
+app.use(contactRouters);
+app.use(successRouters);
 app.use(shopRouters);
 
 // If path is not exist them show error 404 page not found
 app.use((req, res, next) => {
-  console.log(req.path)
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(4000);
--- routes/contact.js
+++ routes/contact.js
@@ -3,13 +3,13 @@ const path = require("path");
 const rootDir = require("../util/path")
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+router.get("/contact", (req, res, next) => {
+    res.sendFile(path.join(rootDir, "views", "contact.html"));
 });
 
-router.post("/add-product", (req, res, next) => {
+router.post("/contact", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect("/success");
 });
 
 module.exports = router;
