9cdd0925796c9b46335757f79dc9d5f98d8d6fd0
KingOfKings01 <aasifkhan9605@gmail.com>
2024-07-31 14:00:59 +0530

    Create add-product with form and prouducts page that shows details using body-parser.

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-const http = require("http")
-const express = require("express")
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.method, req.url)
-  next()
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.send({ key1: "value" })
-  // res.send("<h1>value</h1>")
-})
+app.use("/add-product", (req, res, next) => {
+  res.send(`<form action='/products' method='POST'>
+              <h1>Add Product Form</h1>
+              <div>
+                <label>Title</label>  
+                <input type='text' name='title'>
+              </div>
+              <br>
+              <div>
+              <label>Size</label>  
+              <input type='number' min="0.1" step="0.1" name='size'>
+              </div>
+              <br>
+              <input type='submit'>
+            </form>`);
+});
 
-app.listen(4000)
+app.post("/products", (req, res, next) => {
+  const {title, size}= req.body
+  console.log(`title=${title} size=${size}`);
+  res.send(`<h1>Products Page</h1><p><b>Title:</b> ${title}</p><p><b>Size:</b> ${size}</p>`);
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(4000);
